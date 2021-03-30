3c8f10eff612a2d9ecf564ec45851561a9bec373
alekstar79 <alekstar79@yandex.ru>
2021-03-30 16:28:08 +0400

    Add config

--- cmd/workshop/main.go
+++ cmd/workshop/main.go
@@ -3,13 +3,21 @@ package main
 import (
 	"log"
 	"net/http"
-	
-	"github.com/go-chi/chi"
 
+	"github.com/go-chi/chi"
+	"github.com/ilyakaznacheev/cleanenv"
+
+	"workshop/internal/config"
 	"workshop/internal/handler"
 )
 
 func main() {
+	cfg := config.Server{}
+	err := cleanenv.ReadConfig("config.yml", &cfg)
+
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	h := handler.NewHandler()
 	r := chi.NewRouter()
--- go.mod
+++ go.mod
@@ -4,4 +4,7 @@ go 1.16
 
 require (
 	github.com/go-chi/chi v4.1.1+incompatible
+	github.com/ilyakaznacheev/cleanenv v1.2.5 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+	olympos.io/encoding/edn v0.0.0-20201019073823-d3554ca0b0a3 // indirect
 )
